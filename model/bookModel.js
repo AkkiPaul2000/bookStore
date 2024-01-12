edea2f214d13277ed751ae30f955b01fecbbe107
AkkiPaul2000 <skypaul2000@gmail.com>
2024-01-12 18:08:03 +0530

    model changed!(number to string in price

--- model/bookModel.js
+++ model/bookModel.js
@@ -5,7 +5,7 @@ const bookSchema=mongoose.Schema({
     description:{type:String,required:true},
     author:{type:String,required:true},
     image:{type:String,required:true},
-    price:{type:Number,required:true},
+    price:{type:String,required:true},
 })
 
 module.exports=new mongoose.model("books",bookSchema)
